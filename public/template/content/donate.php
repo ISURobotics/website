d8c000f61ffa71e51170468d4e8ede2e3782beb3
JACOB PRATT <isuandme@gmail.com>
2018-05-24 22:56:56 -0500

    updated the donations page.

--- public/template/content/donate.php
+++ public/template/content/donate.php
@@ -3,22 +3,28 @@
         <h2>
             DONATE
         </h2>
-        <div class="row" style="margin-bottom: 100px;">
+
+
+        <div class="row" style="margin-bottom: 50px;">
             <h4> Through the help of our sponsors, we are able to build projects, provide outreach events, travel to competitions, and grow as a team. Since our club is project based, and each project requires both monetary and gifts-in-kind donations, it is vitally important that we reach out for any and all help in-order for our club to grow. Please allow us to state in advance that we are very grateful for any contributions you have made in the past and will be making in the future.  </h4>
         </div>
         <div class="row" style="margin-bottom: 100px;">
         <h4> Our club is willing to work with any and all sponsors in a collaborative effort to represent them. Some examples of representation have been sponsor logos on shirts, logos and links on our site, and logos on our banners and posters. Similarly, during our competition presentations, we always make it a point to share who were our sponsors.  </h4>
         </div>
-        <div class="row" style="margin-bottom: 100px;">
+
+
+
+
+        <div class="row" style="margin-bottom: 50px;">
             <h4 align="left"> Level 1: &emsp; $1,000 and greater <br>
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on the back of our t-shirts
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on our website (links, logo, description)
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on all promotional posters and banners
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented during all presentations
-                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Don company appearal, host campus visits, or other alternative suggestions
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Don company appearal, company / campus visits, or other alternative suggestions
             </h4>
         </div>
-        <div class="row" style="margin-bottom: 100px;">
+        <div class="row" style="margin-bottom: 50px;">
             <h4 align="left"> Level 2: &emsp; $500 and greater <br>
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on the back of our t-shirts
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on our website (links, logo)
@@ -26,7 +32,7 @@
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented during all presentations
             </h4>
         </div>
-        <div class="row" style="margin-bottom: 100px;">
+        <div class="row" style="margin-bottom: 50px;">
             <h4 align="left"> Level 3: &emsp; $250 and greater <br>
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on our website (logo)
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on all promotional posters and banners
@@ -39,5 +45,21 @@
                 <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented during all presentations
             </h4>
         </div>
+
+
+
+
+        <div class="row" style="margin-bottom: 50px;">
+            <h4 align="left"> Cash: <p style="margin-left: 75px;"> Cash – and cash equivalents such as checks and credit cards – immediately benefit the Iowa State Robotics Club, and is often the most convenient and easiest form of giving. </p>
+            </h4>
+        </div>
+        <div class="row" style="margin-bottom: 100px;">
+            <h4 align="left"> Gifts-in-kind: <p style="margin-left: 75px;"> Gifts-in-kind are items of property such as art, books, computer equipment, patents or livestock. When donated for related educational purposes, they are recognized at their fair market value. An independent appraisal may be required for gifts-in-kind valued at more than $5,000. </p>
+            </h4>
+        </div>
+
+
+
+
     </div>
 </section>

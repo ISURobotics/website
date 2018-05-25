09165483fcd6a1763e37049a0deb72ba45bd2b77
JACOB PRATT <isuandme@gmail.com>
2018-05-24 21:51:53 -0500

    updated the Donations page so that is represents the different tier of donations.

--- public/template/content/donate.php
+++ public/template/content/donate.php
@@ -9,5 +9,35 @@
         <div class="row" style="margin-bottom: 100px;">
         <h4> Our club is willing to work with any and all sponsors in a collaborative effort to represent them. Some examples of representation have been sponsor logos on shirts, logos and links on our site, and logos on our banners and posters. Similarly, during our competition presentations, we always make it a point to share who were our sponsors.  </h4>
         </div>
+        <div class="row" style="margin-bottom: 100px;">
+            <h4 align="left"> Level 1: &emsp; $1,000 and greater <br>
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on the back of our t-shirts
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on our website (links, logo, description)
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on all promotional posters and banners
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented during all presentations
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Don company appearal, host campus visits, or other alternative suggestions
+            </h4>
+        </div>
+        <div class="row" style="margin-bottom: 100px;">
+            <h4 align="left"> Level 2: &emsp; $500 and greater <br>
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on the back of our t-shirts
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on our website (links, logo)
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on all promotional posters and banners
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented during all presentations
+            </h4>
+        </div>
+        <div class="row" style="margin-bottom: 100px;">
+            <h4 align="left"> Level 3: &emsp; $250 and greater <br>
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on our website (logo)
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on all promotional posters and banners
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented during all presentations
+            </h4>
+        </div>
+        <div class="row" style="margin-bottom: 100px;">
+            <h4 align="left"> Level 4: &emsp; $100 and greater <br>
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented on all promotional posters and banners
+                <br> &emsp; &emsp; &emsp; &emsp; &emsp; &bull; Company represented during all presentations
+            </h4>
+        </div>
     </div>
 </section>
